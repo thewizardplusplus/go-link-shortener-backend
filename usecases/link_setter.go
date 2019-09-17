797df291e6f56d11c9450455723c6a25b80a9de2
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-17 23:00:00 +0300

    Add the usecases.LinkSetterGroup.SetLink() method

--- usecases/link_setter.go
+++ usecases/link_setter.go
@@ -1,6 +1,7 @@
 package usecases
 
 import (
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
@@ -13,3 +14,14 @@ type LinkSetter interface {
 
 // LinkSetterGroup ...
 type LinkSetterGroup []LinkSetter
+
+// SetLink ...
+func (setters LinkSetterGroup) SetLink(link entities.Link) error {
+	for _, setter := range setters {
+		if err := setter.SetLink(link); err != nil {
+			return errors.Wrap(err, "unable to set the link")
+		}
+	}
+
+	return nil
+}
