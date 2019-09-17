d524ca974963146033c6a9c20870d84160a744a6
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-17 23:00:00 +0300

    Generate a mock for the usecases.LinkGetter interface

--- usecases/link_getter.go
+++ usecases/link_getter.go
@@ -4,6 +4,8 @@ import (
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
+//go:generate mockery -name=LinkGetter -inpkg -case=underscore -testonly
+
 // LinkGetter ...
 type LinkGetter interface {
 	GetLink(query string) (entities.Link, error)
