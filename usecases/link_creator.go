bca0dbb3ef2f517c3dab2e3f58c53958244eef11
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-17 23:00:00 +0300

    Generate a mock for the usecases.CodeGenerator interface

--- usecases/link_creator.go
+++ usecases/link_creator.go
@@ -1,5 +1,7 @@
 package usecases
 
+//go:generate mockery -name=CodeGenerator -inpkg -case=underscore -testonly
+
 // CodeGenerator ...
 type CodeGenerator interface {
 	GenerateCode() (string, error)
