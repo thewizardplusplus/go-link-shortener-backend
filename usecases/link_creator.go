d6f16ef0f469b9c95b9c8ef5f0407d6ab4ccffe3
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-17 23:00:00 +0300

    Add the usecases.LinkCreator.CreateLink() method

--- usecases/link_creator.go
+++ usecases/link_creator.go
@@ -1,5 +1,12 @@
 package usecases
 
+import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/go-link-shortener/entities"
+)
+
 //go:generate mockery -name=CodeGenerator -inpkg -case=underscore -testonly
 
 // CodeGenerator ...
@@ -12,4 +19,28 @@ type LinkCreator struct {
 	LinkGetter    LinkGetter
 	LinkSetter    LinkSetter
 	CodeGenerator CodeGenerator
+}
+
+// CreateLink ...
+func (creator LinkCreator) CreateLink(url string) (entities.Link, error) {
+	link, err := creator.LinkGetter.GetLink(url)
+	switch err {
+	case nil:
+		return link, nil
+	case sql.ErrNoRows:
+	default:
+		return entities.Link{}, errors.Wrap(err, "unable to get the link")
+	}
+
+	code, err := creator.CodeGenerator.GenerateCode()
+	if err != nil {
+		return entities.Link{}, errors.Wrap(err, "unable to generate a code")
+	}
+
+	link = entities.Link{Code: code, URL: url}
+	if err := creator.LinkSetter.SetLink(link); err != nil {
+		return entities.Link{}, errors.Wrap(err, "unable to set the link")
+	}
+
+	return link, nil
 }
