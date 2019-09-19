4d29805f908042d6f968cc9f9864b7155efb2d69
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-19 23:00:00 +0300

    Add the code.UUIDGenerator.GenerateCode() method

--- code/uuid_generator.go
+++ code/uuid_generator.go
@@ -1,4 +1,19 @@
 package code
 
+import (
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+)
+
 // UUIDGenerator ...
 type UUIDGenerator struct{}
+
+// GenerateCode ...
+func (generator UUIDGenerator) GenerateCode() (string, error) {
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return "", errors.Wrap(err, "unable to generate an UUID V4")
+	}
+
+	return uuid.String(), nil
+}
