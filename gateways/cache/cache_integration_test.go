72289e55c037d10482472c5a4dace95f0727a017
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-19 23:00:00 +0300

    Implement tests of the cache.Cache.GetLink() method

--- gateways/cache/cache_integration_test.go
+++ gateways/cache/cache_integration_test.go
@@ -3,12 +3,25 @@
 package cache
 
 import (
+	"os"
 	"testing"
 
 	"github.com/go-redis/redis"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
+
+var (
+	address string
+)
+
+func init() {
+	var ok bool
+	if address, ok = os.LookupEnv("REDIS_URL"); !ok {
+		address = "localhost:6379"
+	}
+}
 
 func TestCache_GetLink(test *testing.T) {
 	type fields struct {
@@ -26,7 +39,45 @@ func TestCache_GetLink(test *testing.T) {
 		wantLink entities.Link
 		wantErr  assert.ErrorAssertionFunc
 	}{
-		// TODO: add test cases
+		{
+			name: "success",
+			fields: fields{
+				client: redis.NewClient(&redis.Options{Addr: address}),
+			},
+			prepare: func(test *testing.T, client *redis.Client) {
+				err := client.Set("query", `{"Code":"code","URL":"url"}`, 0).Err()
+				require.NoError(test, err)
+			},
+			args:     args{"query"},
+			wantLink: entities.Link{Code: "code", URL: "url"},
+			wantErr:  assert.NoError,
+		},
+		{
+			name: "error without data",
+			fields: fields{
+				client: redis.NewClient(&redis.Options{Addr: address}),
+			},
+			prepare: func(test *testing.T, client *redis.Client) {
+				err := client.Del("query").Err()
+				require.NoError(test, err)
+			},
+			args:     args{"query"},
+			wantLink: entities.Link{},
+			wantErr:  assert.Error,
+		},
+		{
+			name: "error with incorrect data",
+			fields: fields{
+				client: redis.NewClient(&redis.Options{Addr: address}),
+			},
+			prepare: func(test *testing.T, client *redis.Client) {
+				err := client.Set("query", "incorrect", 0).Err()
+				require.NoError(test, err)
+			},
+			args:     args{"query"},
+			wantLink: entities.Link{},
+			wantErr:  assert.Error,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			data.prepare(test, data.fields.client)
