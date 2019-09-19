b460c022a8ef9c4ff1e731b9e344a929ab631240
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-09-19 23:00:00 +0300

    Add the cache.Cache structure

--- gateways/cache/cache.go
+++ gateways/cache/cache.go
@@ -1,8 +1,18 @@
 package cache
 
 import (
+	"time"
+
+	"github.com/go-redis/redis"
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
 // KeyExtractor ...
 type KeyExtractor func(link entities.Link) string
+
+// Cache ...
+type Cache struct {
+	client     *redis.Client
+	expiration time.Duration
+	key        KeyExtractor
+}
