21f088a390ce907ea01459231f98fd3eb1d6985d
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-01 23:00:00 +0300

    Add the storage.NewClient() function

--- gateways/storage/client.go
+++ gateways/storage/client.go
@@ -1,10 +1,26 @@
 package storage
 
 import (
+	"context"
+
+	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Client ...
 type Client struct {
 	innerClient *mongo.Client
+}
+
+// NewClient ...
+func NewClient(uri string) (Client, error) {
+	context := context.Background()
+	options := options.Client().ApplyURI(uri)
+	innerClient, err := mongo.Connect(context, options)
+	if err != nil {
+		return Client{}, errors.Wrap(err, "unable to connect to MongoDB")
+	}
+
+	return Client{innerClient: innerClient}, nil
 }
