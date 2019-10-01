af5bb420aab4ac8ee115c622a5de8eee18db4a4d
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-01 23:00:00 +0300

    Add the cache.LinkSetter.SetLink() method

--- gateways/cache/link_setter.go
+++ gateways/cache/link_setter.go
@@ -1,7 +1,11 @@
 package cache
 
 import (
+	"encoding/json"
 	"time"
+
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
 // LinkSetter ...
@@ -9,4 +13,21 @@ type LinkSetter struct {
 	KeyExtractor KeyExtractor
 	Client       Client
 	Expiration   time.Duration
+}
+
+// SetLink ...
+func (setter LinkSetter) SetLink(link entities.Link) error {
+	data, err := json.Marshal(link)
+	if err != nil {
+		return errors.Wrap(err, "unable to marshal the link for Redis")
+	}
+
+	key := setter.KeyExtractor(link)
+	if err := setter.Client.innerClient.
+		Set(key, string(data), setter.Expiration).
+		Err(); err != nil {
+		return errors.Wrap(err, "unable to set the link in Redis")
+	}
+
+	return nil
 }
