bb5ec9d36a7d86fa2dff8e0db1f00eb1c3afdd1f
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-02 23:00:00 +0300

    Add the storage.LinkSetter.SetLink() method

--- gateways/storage/link_setter.go
+++ gateways/storage/link_setter.go
@@ -1,8 +1,28 @@
 package storage
 
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/thewizardplusplus/go-link-shortener/entities"
+)
+
 // LinkSetter ...
 type LinkSetter struct {
 	Client     Client
 	Database   string
 	Collection string
+}
+
+// SetLink ...
+func (setter LinkSetter) SetLink(link entities.Link) error {
+	_, err := setter.Client.innerClient.
+		Database(setter.Database).
+		Collection(setter.Collection).
+		InsertOne(context.Background(), link)
+	if err != nil {
+		return errors.Wrap(err, "unable to set the link in MongoDB")
+	}
+
+	return nil
 }
