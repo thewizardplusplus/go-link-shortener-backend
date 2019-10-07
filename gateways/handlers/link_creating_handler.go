f1b0d4aad8c56ce7b471835af01def069d4dc7ac
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-07 23:00:00 +0300

    Add the handlers.LinkCreatingHandler.ServeHTTP() method

--- gateways/handlers/link_creating_handler.go
+++ gateways/handlers/link_creating_handler.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
@@ -16,4 +20,22 @@ type LinkCreatingHandler struct {
 	LinkCreator    LinkCreator
 	LinkPresenter  LinkPresenter
 	ErrorPresenter ErrorPresenter
+}
+
+// ServeHTTP ...
+func (handler LinkCreatingHandler) ServeHTTP(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+	url := mux.Vars(request)["url"]
+	link, err := handler.LinkCreator.CreateLink(url)
+	if err != nil {
+		const statusCode = http.StatusInternalServerError
+		err = errors.Wrap(err, "unable to create the link")
+		handler.ErrorPresenter.PresentError(writer, statusCode, err)
+
+		return
+	}
+
+	handler.LinkPresenter.PresentLink(writer, link)
 }
