ebe36f94f363e42c97648848f5874d5f8181fbae
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-07 23:00:00 +0300

    Implement tests of the handlers.LinkCreatingHandler.ServeHTTP() method

--- gateways/handlers/link_creating_handler_test.go
+++ gateways/handlers/link_creating_handler_test.go
@@ -5,9 +5,12 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"testing/iotest"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
 func TestLinkCreatingHandler_ServeHTTP(test *testing.T) {
@@ -25,7 +28,69 @@ func TestLinkCreatingHandler_ServeHTTP(test *testing.T) {
 		fields fields
 		args   args
 	}{
-		// TODO: add test cases
+		{
+			name: "success",
+			fields: fields{
+				LinkCreator: func() LinkCreator {
+					creator := new(MockLinkCreator)
+					creator.
+						On("CreateLink", "url").
+						Return(entities.Link{Code: "code", URL: "url"}, nil)
+
+					return creator
+				}(),
+				LinkPresenter: func() LinkPresenter {
+					presenter := new(MockLinkPresenter)
+					presenter.On(
+						"PresentLink",
+						mock.MatchedBy(func(writer http.ResponseWriter) bool { return true }),
+						entities.Link{Code: "code", URL: "url"},
+					)
+
+					return presenter
+				}(),
+				ErrorPresenter: new(MockErrorPresenter),
+			},
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodPost, "http://example.com/", nil)
+					request = mux.SetURLVars(request, map[string]string{"url": "url"})
+
+					return request
+				}(),
+			},
+		},
+		{
+			name: "error with creating",
+			fields: fields{
+				LinkCreator: func() LinkCreator {
+					creator := new(MockLinkCreator)
+					creator.On("CreateLink", "url").Return(entities.Link{}, iotest.ErrTimeout)
+
+					return creator
+				}(),
+				LinkPresenter: new(MockLinkPresenter),
+				ErrorPresenter: func() ErrorPresenter {
+					presenter := new(MockErrorPresenter)
+					presenter.On(
+						"PresentError",
+						mock.MatchedBy(func(writer http.ResponseWriter) bool { return true }),
+						http.StatusInternalServerError,
+						mock.MatchedBy(func(err error) bool { return true }),
+					)
+
+					return presenter
+				}(),
+			},
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodPost, "http://example.com/", nil)
+					request = mux.SetURLVars(request, map[string]string{"url": "url"})
+
+					return request
+				}(),
+			},
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			writer := httptest.NewRecorder()
