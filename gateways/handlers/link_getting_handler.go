d1f4466aba97dfdcaf91c486149c96b80ac4e876
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-07 23:00:00 +0300

    Generate a mock for the handlers.ErrorPresenter interface

--- gateways/handlers/link_getting_handler.go
+++ gateways/handlers/link_getting_handler.go
@@ -20,6 +20,8 @@ type LinkPresenter interface {
 	PresentLink(writer http.ResponseWriter, link entities.Link)
 }
 
+//go:generate mockery -name=ErrorPresenter -inpkg -case=underscore -testonly
+
 // ErrorPresenter ...
 type ErrorPresenter interface {
 	PresentError(writer http.ResponseWriter, statusCode int, err error)
