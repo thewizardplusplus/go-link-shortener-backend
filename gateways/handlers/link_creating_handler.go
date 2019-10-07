7dc8a320e7ca2b4a0a7d19c21c61821417561a77
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-07 23:00:00 +0300

    Generate a mock for the handlers.LinkCreator interface

--- gateways/handlers/link_creating_handler.go
+++ gateways/handlers/link_creating_handler.go
@@ -4,6 +4,8 @@ import (
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
+//go:generate mockery -name=LinkCreator -inpkg -case=underscore -testonly
+
 // LinkCreator ...
 type LinkCreator interface {
 	CreateLink(url string) (entities.Link, error)
