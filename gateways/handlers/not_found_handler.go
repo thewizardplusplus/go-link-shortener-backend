8c24ac3be41d8adb1730e10b3b2c1649973040de
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add the handlers.NotFoundHandler.ServeHTTP() method

--- gateways/handlers/not_found_handler.go
+++ gateways/handlers/not_found_handler.go
@@ -1,6 +1,22 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
 // NotFoundHandler ...
 type NotFoundHandler struct {
 	ErrorPresenter ErrorPresenter
+}
+
+// ServeHTTP ...
+func (handler NotFoundHandler) ServeHTTP(
+	writer http.ResponseWriter,
+	request *http.Request,
+) {
+	const statusCode = http.StatusNotFound
+	err := errors.New("unable to find the endpoint")
+	handler.ErrorPresenter.PresentError(writer, statusCode, err)
 }
