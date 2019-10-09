2bc6faddac31d125bbf06bb8bf399d8d4ec15200
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add the presenters.presentData() function

--- gateways/presenters/json_presenter.go
+++ gateways/presenters/json_presenter.go
@@ -1,4 +1,18 @@
 package presenters
 
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
 // JSONPresenter ...
 type JSONPresenter struct{}
+
+func presentData(writer http.ResponseWriter, statusCode int, data interface{}) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(statusCode)
+
+	bytes, _ := json.Marshal(data)        // nolint: gosec
+	io.WriteString(writer, string(bytes)) // nolint: gosec, errcheck
+}
