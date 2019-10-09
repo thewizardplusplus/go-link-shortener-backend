2f562c0b13ab2eecfa32c0f40487c5ab8fcbcfa4
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add the router.NewRouter() function

--- gateways/router/router.go
+++ gateways/router/router.go
@@ -2,6 +2,8 @@ package router
 
 import (
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Handlers ...
@@ -9,4 +11,15 @@ type Handlers struct {
 	LinkGettingHandler  http.Handler
 	LinkCreatingHandler http.Handler
 	NotFoundHandler     http.Handler
+}
+
+// NewRouter ...
+func NewRouter(handlers Handlers) http.Handler {
+	router := mux.NewRouter().PathPrefix("/api/v1/links").Subrouter()
+	router.NotFoundHandler = handlers.NotFoundHandler
+	router.MethodNotAllowedHandler = handlers.NotFoundHandler
+	router.Handle("/{code}", handlers.LinkGettingHandler).Methods(http.MethodGet)
+	router.Handle("/", handlers.LinkCreatingHandler).Methods(http.MethodPost)
+
+	return router
 }
