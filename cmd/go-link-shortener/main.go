860f7fabc7a767700f1e4d3efd86fe628480b269
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Implement the general handler

--- cmd/go-link-shortener/main.go
+++ cmd/go-link-shortener/main.go
@@ -2,25 +2,90 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
+
+	"github.com/thewizardplusplus/go-link-shortener/code"
+	"github.com/thewizardplusplus/go-link-shortener/entities"
+	"github.com/thewizardplusplus/go-link-shortener/gateways/cache"
+	"github.com/thewizardplusplus/go-link-shortener/gateways/handlers"
+	"github.com/thewizardplusplus/go-link-shortener/gateways/presenters"
+	"github.com/thewizardplusplus/go-link-shortener/gateways/router"
+	"github.com/thewizardplusplus/go-link-shortener/gateways/storage"
+	"github.com/thewizardplusplus/go-link-shortener/usecases"
 )
 
 const (
-	serverAddress = ":8080"
+	serverAddress     = ":8080"
+	cacheAddress      = "localhost:6379"
+	storageAddress    = "mongodb://localhost:27017"
+	storageDatabase   = "go-link-shortener"
+	storageCollection = "links"
 )
 
 func main() {
+	cacheClient := cache.NewClient(cacheAddress)
+	cacheGetter := cache.LinkGetter{Client: cacheClient}
+
+	storageClient, err := storage.NewClient(storageAddress)
+	if err != nil {
+		log.Fatalf("error on creating the storage client: %v", err)
+	}
+
+	var presenter presenters.JSONPresenter
 	server := http.Server{
 		Addr: serverAddress,
-		Handler: http.HandlerFunc(func(
-			writer http.ResponseWriter,
-			request *http.Request,
-		) {
-			io.WriteString(writer, "Hello, world!\n")
+		Handler: router.NewRouter(router.Handlers{
+			LinkGettingHandler: handlers.LinkGettingHandler{
+				LinkGetter: usecases.LinkGetterGroup{
+					cacheGetter,
+					storage.LinkGetter{
+						Client:     storageClient,
+						Database:   storageDatabase,
+						Collection: storageCollection,
+						KeyField:   "code",
+					},
+				},
+				LinkPresenter:  presenter,
+				ErrorPresenter: presenter,
+			},
+			LinkCreatingHandler: handlers.LinkCreatingHandler{
+				LinkCreator: usecases.LinkCreator{
+					LinkGetter: usecases.LinkGetterGroup{
+						cacheGetter,
+						storage.LinkGetter{
+							Client:     storageClient,
+							Database:   storageDatabase,
+							Collection: storageCollection,
+							KeyField:   "url",
+						},
+					},
+					LinkSetter: usecases.LinkSetterGroup{
+						cache.LinkSetter{
+							KeyExtractor: func(link entities.Link) string { return link.Code },
+							Client:       cacheClient,
+							Expiration:   time.Hour,
+						},
+						cache.LinkSetter{
+							KeyExtractor: func(link entities.Link) string { return link.URL },
+							Client:       cacheClient,
+							Expiration:   time.Hour,
+						},
+						storage.LinkSetter{
+							Client:     storageClient,
+							Database:   storageDatabase,
+							Collection: storageCollection,
+						},
+					},
+					CodeGenerator: code.UUIDGenerator{},
+				},
+				LinkPresenter:  presenter,
+				ErrorPresenter: presenter,
+			},
+			NotFoundHandler: handlers.NotFoundHandler{ErrorPresenter: presenter},
 		}),
 	}
 
