0a0f3dad975caee847994f949578b88134822074
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add the presenters.JSONPresenter.PresentLink() method

--- gateways/presenters/json_presenter.go
+++ gateways/presenters/json_presenter.go
@@ -4,10 +4,20 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+
+	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
 // JSONPresenter ...
 type JSONPresenter struct{}
+
+// PresentLink ...
+func (presenter JSONPresenter) PresentLink(
+	writer http.ResponseWriter,
+	link entities.Link,
+) {
+	presentData(writer, http.StatusOK, link)
+}
 
 func presentData(writer http.ResponseWriter, statusCode int, data interface{}) {
 	writer.Header().Set("Content-Type", "application/json")
