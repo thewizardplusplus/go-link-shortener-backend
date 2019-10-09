ea78b9828a1e8161e36d460cc147143d3d7c0ccd
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add tests for the presenters.JSONPresenter.PresentError() method

--- gateways/presenters/json_presenter_test.go
+++ gateways/presenters/json_presenter_test.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"testing/iotest"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/thewizardplusplus/go-link-shortener/entities"
@@ -21,4 +22,18 @@ func TestJSONPresenter_PresentLink(test *testing.T) {
 	assert.Equal(test, http.StatusOK, response.StatusCode)
 	assert.Equal(test, "application/json", response.Header.Get("Content-Type"))
 	assert.Equal(test, `{"Code":"code","URL":"url"}`, string(responseBody))
+}
+
+func TestJSONPresenter_PresentError(test *testing.T) {
+	writer := httptest.NewRecorder()
+	presenter := JSONPresenter{}
+	presenter.
+		PresentError(writer, http.StatusInternalServerError, iotest.ErrTimeout)
+
+	response := writer.Result()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+
+	assert.Equal(test, http.StatusInternalServerError, response.StatusCode)
+	assert.Equal(test, "application/json", response.Header.Get("Content-Type"))
+	assert.Equal(test, `{"Error":"timeout"}`, string(responseBody))
 }
