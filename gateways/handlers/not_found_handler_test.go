f29eeb3e1f7f57fa7534280df50c73a1a5a3eb47
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-09 23:00:00 +0300

    Add tests for the handlers.NotFoundHandler.ServeHTTP() method

---
+++ gateways/handlers/not_found_handler_test.go
@@ -0,0 +1,34 @@
+package handlers
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestNotFoundHandler_ServeHTTP(test *testing.T) {
+	presenter := new(MockErrorPresenter)
+	presenter.On(
+		"PresentError",
+		mock.MatchedBy(func(writer http.ResponseWriter) bool { return true }),
+		http.StatusNotFound,
+		mock.MatchedBy(func(err error) bool { return true }),
+	)
+
+	writer := httptest.NewRecorder()
+	request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+	handler := NotFoundHandler{
+		ErrorPresenter: presenter,
+	}
+	handler.ServeHTTP(writer, request)
+
+	response := writer.Result()
+	responseBody, _ := ioutil.ReadAll(response.Body)
+
+	mock.AssertExpectationsForObjects(test, presenter)
+	assert.Empty(test, responseBody)
+}
