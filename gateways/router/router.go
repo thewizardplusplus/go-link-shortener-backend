c3035fa1dfcaff074978b2384046fe9a1246fcfb
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-11 23:00:00 +0300

    Describe the general API info

--- gateways/router/router.go
+++ gateways/router/router.go
@@ -15,6 +15,12 @@ type Handlers struct {
 
 // NewRouter ...
 func NewRouter(handlers Handlers) http.Handler {
+	// @title go-link-shortener API
+	// @version 1.0.0
+	// @license.name MIT
+	// @host localhost:8080
+	// @basePath /api/v1
+
 	router := mux.NewRouter().PathPrefix("/api/v1/links").Subrouter()
 	router.NotFoundHandler = handlers.NotFoundHandler
 	router.MethodNotAllowedHandler = handlers.NotFoundHandler
