df5738a5f98a53f243a33613fcf05f688bf6c1a6
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-18 23:00:00 +0300

    Implement tests of API errors

--- tests/errors_test.go
+++ tests/errors_test.go
@@ -7,18 +7,38 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/caarlos0/env"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/thewizardplusplus/go-link-shortener/gateways/presenters"
 )
 
 func TestErrors(test *testing.T) {
+	type options struct {
+		ServerAddress string `env:"SERVER_ADDRESS" envDefault:"http://localhost:8080"`
+	}
+
+	var opts options
+	err := env.Parse(&opts)
+	require.NoError(test, err)
+
 	for _, data := range []struct {
 		name       string
 		request    *http.Request
 		wantStatus int
 	}{
-		// TODO: add test cases
+		{
+			name: "unknown endpoint",
+			request: func() *http.Request {
+				request, _ := http.NewRequest(
+					http.MethodGet,
+					opts.ServerAddress+"/api/v1/incorrect",
+					nil,
+				)
+				return request
+			}(),
+			wantStatus: http.StatusNotFound,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			response, err := http.DefaultClient.Do(data.request)
