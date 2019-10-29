ca53d8d53e8ddd4b7692235a5b7a052878e5c606
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-10-29 23:00:00 +0300

    Add the code.DistributedCounter interface

--- code/distributed_generator.go
+++ code/distributed_generator.go
@@ -4,3 +4,8 @@ package code
 //
 // It should NOT be concurrently safe, because it'll call only under lock.
 type RandomSource func(maximum int) int
+
+// DistributedCounter ...
+type DistributedCounter interface {
+	NextCountChunk(counterName string) (uint64, error)
+}
