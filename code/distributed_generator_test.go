89c386938db9016b079073c1002d2fdd81706fa4
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-11-01 23:00:00 +0300

    Add tests for the code.DistributedGenerator.selectCounter() method

--- code/distributed_generator_test.go
+++ code/distributed_generator_test.go
@@ -26,6 +26,31 @@ func TestNewDistributedGenerator(test *testing.T) {
 	assert.Equal(test, getPointer(rand.Intn), getPointer(got.randomSource))
 }
 
+func TestDistributedGenerator_selectCounter(test *testing.T) {
+	type markedDistributedCounter struct {
+		MockDistributedCounter
+
+		ID int
+	}
+
+	distributedCounters := []DistributedCounter{
+		&markedDistributedCounter{ID: 1},
+		&markedDistributedCounter{ID: 2},
+	}
+	randomSource := rand.New(rand.NewSource(1))
+	generator := &DistributedGenerator{
+		counter:             chunkedCounter{step: 23},
+		distributedCounters: distributedCounters,
+		randomSource:        randomSource.Intn,
+	}
+	got := generator.selectCounter()
+
+	for _, distributedCounter := range distributedCounters {
+		mock.AssertExpectationsForObjects(test, distributedCounter)
+	}
+	assert.Equal(test, &markedDistributedCounter{ID: 2}, got)
+}
+
 func getPointer(value interface{}) uintptr {
 	return reflect.ValueOf(value).Pointer()
 }
