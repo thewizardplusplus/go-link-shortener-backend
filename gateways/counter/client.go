d438f08027418d4a2d52277f56f7e2dd20e21779
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-11-05 23:00:00 +0300

    Add the counter.NewClient() function

--- gateways/counter/client.go
+++ gateways/counter/client.go
@@ -1,10 +1,21 @@
 package counter
 
 import (
+	"github.com/pkg/errors"
 	"go.etcd.io/etcd/clientv3"
 )
 
 // Client ...
 type Client struct {
 	innerClient *clientv3.Client
+}
+
+// NewClient ...
+func NewClient(url string) (Client, error) {
+	innerClient, err := clientv3.New(clientv3.Config{Endpoints: []string{url}})
+	if err != nil {
+		return Client{}, errors.Wrap(err, "unable to connect to etcd")
+	}
+
+	return Client{innerClient: innerClient}, nil
 }
