8edc6015197da19577694c78b8745d23b23f27b1
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-11-07 23:00:00 +0300

    Implements tests of the counter.Counter.NextCountChunk() method

--- gateways/counter/counter_test.go
+++ gateways/counter/counter_test.go
@@ -3,16 +3,26 @@
 package counter
 
 import (
+	"context"
 	"testing"
 
+	"github.com/caarlos0/env"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCounter_NextCountChunk(test *testing.T) {
+	type options struct {
+		CounterAddress string `env:"COUNTER_ADDRESS" envDefault:"localhost:2379"`
+	}
 	type fields struct {
 		makeClient func(test *testing.T) Client
 		name       string
 	}
+
+	var opts options
+	err := env.Parse(&opts)
+	require.NoError(test, err)
 
 	for _, data := range []struct {
 		name    string
@@ -21,7 +31,29 @@ func TestCounter_NextCountChunk(test *testing.T) {
 		wantErr assert.ErrorAssertionFunc
 		check   func(test *testing.T, preparedData interface{}, gotChunk uint64)
 	}{
-		// TODO: add test cases
+		{
+			name: "success",
+			prepare: func(test *testing.T, counter Counter) (preparedData interface{}) {
+				context := context.Background()
+				response, err := counter.Client.innerClient.Get(context, "counter")
+				require.NoError(test, err)
+
+				return uint64(response.Header.Revision)
+			},
+			fields: fields{
+				makeClient: func(test *testing.T) Client {
+					client, err := NewClient(opts.CounterAddress)
+					require.NoError(test, err)
+
+					return client
+				},
+				name: "counter",
+			},
+			check: func(test *testing.T, preparedData interface{}, gotChunk uint64) {
+				assert.Equal(test, preparedData.(uint64)+1, gotChunk)
+			},
+			wantErr: assert.NoError,
+		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
 			client := data.fields.makeClient(test)
