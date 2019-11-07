d8260805b9cbed85a9ae86119f0868af3f6511ff
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-11-07 23:00:00 +0300

    Add the counter.Counter.NextCountChunk() method

--- gateways/counter/counter.go
+++ gateways/counter/counter.go
@@ -1,7 +1,24 @@
 package counter
 
+import (
+	"context"
+
+	"github.com/pkg/errors"
+)
+
 // Counter ...
 type Counter struct {
 	Client Client
 	Name   string
+}
+
+// NextCountChunk ...
+func (counter Counter) NextCountChunk() (uint64, error) {
+	context := context.Background()
+	response, err := counter.Client.innerClient.Put(context, counter.Name, "")
+	if err != nil {
+		return 0, errors.Wrap(err, "unable to update the counter")
+	}
+
+	return uint64(response.Header.Revision), nil
 }
