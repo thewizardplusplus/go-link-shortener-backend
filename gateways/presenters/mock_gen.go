5bc11e9a19d56aa019653b90b256d4571570cb75
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-12-18 23:00:00 +0300

    Generate a mock for the http.ResponseWriter interface

---
+++ gateways/presenters/mock_gen.go
@@ -0,0 +1,14 @@
+package presenters
+
+import (
+	"net/http"
+)
+
+//go:generate mockery -name=ResponseWriter -inpkg -case=underscore -testonly
+
+// ResponseWriter ...
+//
+// It's used only for mock generating.
+type ResponseWriter interface {
+	http.ResponseWriter
+}
