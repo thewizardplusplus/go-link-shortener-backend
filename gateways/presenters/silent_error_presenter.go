c7de933be32075bf52f08c39cf41564afb9b458c
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-12-18 23:00:00 +0300

    Generate a mock for the presenters.ErrorPresenter interface

--- gateways/presenters/silent_error_presenter.go
+++ gateways/presenters/silent_error_presenter.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 )
 
+//go:generate mockery -name=ErrorPresenter -inpkg -case=underscore -testonly
+
 // ErrorPresenter ...
 type ErrorPresenter interface {
 	PresentError(writer http.ResponseWriter, statusCode int, err error) error
