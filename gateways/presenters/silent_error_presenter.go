9a575a4790efee6014cf5586060d89b05c80d474
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-12-18 23:00:00 +0300

    Add the presenters.SilentErrorPresenter.PresentError() method

--- gateways/presenters/silent_error_presenter.go
+++ gateways/presenters/silent_error_presenter.go
@@ -16,3 +16,18 @@ type SilentErrorPresenter struct {
 	ErrorPresenter ErrorPresenter
 	Printer        Printer
 }
+
+// PresentError ...
+func (presenter SilentErrorPresenter) PresentError(
+	writer http.ResponseWriter,
+	statusCode int,
+	err error,
+) {
+	if err = presenter.ErrorPresenter.PresentError(
+		writer,
+		statusCode,
+		err,
+	); err != nil {
+		presenter.Printer.Printf("unable to present the error: %v", err)
+	}
+}
