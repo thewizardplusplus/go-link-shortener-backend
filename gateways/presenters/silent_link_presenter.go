aceb2729cc2f51f8a2ea5fe981db8f8a5afc20a0
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-12-18 23:00:00 +0300

    Generate a mock for the presenters.LinkPresenter interface

--- gateways/presenters/silent_link_presenter.go
+++ gateways/presenters/silent_link_presenter.go
@@ -6,6 +6,8 @@ import (
 	"github.com/thewizardplusplus/go-link-shortener/entities"
 )
 
+//go:generate mockery -name=LinkPresenter -inpkg -case=underscore -testonly
+
 // LinkPresenter ...
 type LinkPresenter interface {
 	PresentLink(writer http.ResponseWriter, link entities.Link) error
