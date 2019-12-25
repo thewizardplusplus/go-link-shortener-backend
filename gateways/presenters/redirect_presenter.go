3efb854ea70b9bc09d650f189127fbe0e455ea92
thewizardplusplus <thewizardplusplus@yandex.ru>
2019-12-25 23:00:00 +0300

    Add the presenters.RedirectPresenter.PresentLink() method

--- gateways/presenters/redirect_presenter.go
+++ gateways/presenters/redirect_presenter.go
@@ -1,7 +1,22 @@
 package presenters
 
+import (
+	"net/http"
+
+	"github.com/thewizardplusplus/go-link-shortener/entities"
+)
+
 // RedirectPresenter ...
 type RedirectPresenter struct {
 	ErrorURL string
 	Printer  Printer
+}
+
+// PresentLink ...
+func (presenter RedirectPresenter) PresentLink(
+	writer http.ResponseWriter,
+	request *http.Request,
+	link entities.Link,
+) {
+	http.Redirect(writer, request, link.URL, http.StatusMovedPermanently)
 }
