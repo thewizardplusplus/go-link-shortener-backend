25ba333d54b1e9a707bdb5e88c1061366f5ea0f5
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-01-10 23:00:00 +0300

    Add a base tests for the presenters.RedirectPresenter.PresentError() method

--- gateways/presenters/redirect_presenter_test.go
+++ gateways/presenters/redirect_presenter_test.go
@@ -110,3 +110,41 @@ func TestRedirectPresenter_PresentLink(test *testing.T) {
 		})
 	}
 }
+
+func TestRedirectPresenter_PresentError(test *testing.T) {
+	type fields struct {
+		ErrorURL string
+		Printer  Printer
+	}
+	type args struct {
+		writer     http.ResponseWriter
+		statusCode int
+		err        error
+	}
+
+	for _, data := range []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr assert.ErrorAssertionFunc
+		check   func(test *testing.T, writer http.ResponseWriter)
+	}{
+		// TODO: add test cases
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			presenter := RedirectPresenter{
+				ErrorURL: data.fields.ErrorURL,
+				Printer:  data.fields.Printer,
+			}
+			gotErr := presenter.PresentError(
+				data.args.writer,
+				data.args.statusCode,
+				data.args.err,
+			)
+
+			mock.AssertExpectationsForObjects(test, data.fields.Printer)
+			data.wantErr(test, gotErr)
+			data.check(test, data.args.writer)
+		})
+	}
+}
