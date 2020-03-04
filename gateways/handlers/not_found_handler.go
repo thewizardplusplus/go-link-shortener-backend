6a40b6c8c8fe5122ecfd2fcdc75a7e0580c497d2
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-03-04 23:00:00 +0300

    Fix the handlers.NotFoundHandler structure

--- gateways/handlers/not_found_handler.go
+++ gateways/handlers/not_found_handler.go
@@ -8,7 +8,8 @@ import (
 
 // NotFoundHandler ...
 type NotFoundHandler struct {
-	ErrorPresenter ErrorPresenter
+	TextErrorPresenter ErrorPresenter
+	JSONErrorPresenter ErrorPresenter
 }
 
 // ServeHTTP ...
@@ -16,7 +17,33 @@ func (handler NotFoundHandler) ServeHTTP(
 	writer http.ResponseWriter,
 	request *http.Request,
 ) {
+	var errorPresenter ErrorPresenter
+	if isTextAccepted(request) {
+		errorPresenter = handler.TextErrorPresenter
+	} else {
+		errorPresenter = handler.JSONErrorPresenter
+	}
+
 	const statusCode = http.StatusNotFound
 	err := errors.New("unable to find the endpoint")
-	handler.ErrorPresenter.PresentError(writer, request, statusCode, err)
+	errorPresenter.PresentError(writer, request, statusCode, err)
+}
+
+// based on:
+// * https://create-react-app.dev/docs/proxying-api-requests-in-development
+// * https://github.com/facebook/create-react-app/blob/v3.4.0/packages/react-dev-utils/WebpackDevServerUtils.js#L423
+func isTextAccepted(request *http.Request) bool {
+	if request.Method != http.MethodGet {
+		return false
+	}
+
+	var isTextAccepted bool
+	for _, acceptingType := range request.Header["Accept"] {
+		if acceptingType == "text/html" {
+			isTextAccepted = true
+			break
+		}
+	}
+
+	return isTextAccepted
 }
--- gateways/handlers/not_found_handler_test.go
+++ gateways/handlers/not_found_handler_test.go
@@ -11,26 +11,188 @@ import (
 )
 
 func TestNotFoundHandler_ServeHTTP(test *testing.T) {
-	request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
-
-	presenter := new(MockErrorPresenter)
-	presenter.On(
-		"PresentError",
-		mock.MatchedBy(func(http.ResponseWriter) bool { return true }),
-		request,
-		http.StatusNotFound,
-		mock.MatchedBy(func(error) bool { return true }),
-	)
-
-	writer := httptest.NewRecorder()
-	handler := NotFoundHandler{
-		ErrorPresenter: presenter,
+	type fields struct {
+		TextErrorPresenter ErrorPresenter
+		JSONErrorPresenter ErrorPresenter
 	}
-	handler.ServeHTTP(writer, request)
+	type args struct {
+		request *http.Request
+	}
 
-	response := writer.Result()
-	responseBody, _ := ioutil.ReadAll(response.Body)
+	for _, data := range []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "without the Accept header",
+			fields: fields{
+				TextErrorPresenter: new(MockErrorPresenter),
+				JSONErrorPresenter: func() ErrorPresenter {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
 
-	mock.AssertExpectationsForObjects(test, presenter)
-	assert.Empty(test, responseBody)
+					presenter := new(MockErrorPresenter)
+					presenter.On(
+						"PresentError",
+						mock.MatchedBy(func(http.ResponseWriter) bool { return true }),
+						request,
+						http.StatusNotFound,
+						mock.MatchedBy(func(error) bool { return true }),
+					)
+
+					return presenter
+				}(),
+			},
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+			},
+		},
+		{
+			name: "with `text/html` in the Accept header",
+			fields: fields{
+				TextErrorPresenter: func() ErrorPresenter {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "text/html")
+
+					presenter := new(MockErrorPresenter)
+					presenter.On(
+						"PresentError",
+						mock.MatchedBy(func(http.ResponseWriter) bool { return true }),
+						request,
+						http.StatusNotFound,
+						mock.MatchedBy(func(error) bool { return true }),
+					)
+
+					return presenter
+				}(),
+				JSONErrorPresenter: new(MockErrorPresenter),
+			},
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "text/html")
+
+					return request
+				}(),
+			},
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			writer := httptest.NewRecorder()
+			handler := NotFoundHandler{
+				TextErrorPresenter: data.fields.TextErrorPresenter,
+				JSONErrorPresenter: data.fields.JSONErrorPresenter,
+			}
+			handler.ServeHTTP(writer, data.args.request)
+
+			response := writer.Result()
+			responseBody, _ := ioutil.ReadAll(response.Body)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.fields.TextErrorPresenter,
+				data.fields.JSONErrorPresenter,
+			)
+			assert.Empty(test, responseBody)
+		})
+	}
+}
+
+func Test_isTextAccepted(test *testing.T) {
+	type args struct {
+		request *http.Request
+	}
+
+	for _, data := range []struct {
+		name string
+		args args
+		want assert.BoolAssertionFunc
+	}{
+		{
+			name: "without the Accept header",
+			args: args{
+				request: httptest.NewRequest(http.MethodGet, "http://example.com/", nil),
+			},
+			want: assert.False,
+		},
+		{
+			name: "with `text/html` in the Accept header (GET)",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "text/html")
+
+					return request
+				}(),
+			},
+			want: assert.True,
+		},
+		{
+			name: "with `text/html` in the Accept header (POST)",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodPost, "http://example.com/", nil)
+					request.Header.Add("Accept", "text/html")
+
+					return request
+				}(),
+			},
+			want: assert.False,
+		},
+		{
+			name: "with `application/json` in the Accept header",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "application/json")
+
+					return request
+				}(),
+			},
+			want: assert.False,
+		},
+		{
+			name: "with `application/json` and `text/html` in the Accept header",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "application/json")
+					request.Header.Add("Accept", "text/html")
+
+					return request
+				}(),
+			},
+			want: assert.True,
+		},
+		{
+			name: "with `text/*` in the Accept header",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "text/*")
+
+					return request
+				}(),
+			},
+			want: assert.False,
+		},
+		{
+			name: "with `*/*` in the Accept header",
+			args: args{
+				request: func() *http.Request {
+					request := httptest.NewRequest(http.MethodGet, "http://example.com/", nil)
+					request.Header.Add("Accept", "*/*")
+
+					return request
+				}(),
+			},
+			want: assert.False,
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			got := isTextAccepted(data.args.request)
+
+			data.want(test, got)
+		})
+	}
 }
