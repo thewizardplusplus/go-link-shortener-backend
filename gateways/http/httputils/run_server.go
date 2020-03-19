cdb70bd7d4635cfb73ccb1a63b60ba277cf01734
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-03-19 23:00:00 +0300

    Add the httputils.RunServer() function

---
+++ gateways/http/httputils/run_server.go
@@ -0,0 +1,64 @@
+package httputils
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+//go:generate mockery -name=Server -inpkg -case=underscore -testonly
+
+// Server ...
+type Server interface {
+	ListenAndServe() error
+	Shutdown(ctx context.Context) error
+}
+
+// RunServerDependencies ...
+type RunServerDependencies struct {
+	Server  Server
+	Printer Printer
+}
+
+// RunServer ...
+func RunServer(
+	shutdownCtx context.Context,
+	dependencies RunServerDependencies,
+	interruptSignals ...os.Signal,
+) (ok bool) {
+	var waiter sync.WaitGroup
+	waiter.Add(1)
+
+	interruptCtx, interruptCtxCancel := context.WithCancel(context.Background())
+	defer interruptCtxCancel()
+
+	go func() {
+		defer waiter.Done()
+
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, interruptSignals...)
+
+		select {
+		case <-interrupt:
+		case <-interruptCtx.Done():
+			return
+		}
+
+		err := dependencies.Server.Shutdown(shutdownCtx)
+		if ok = err == nil; !ok {
+			// error with closing listeners
+			dependencies.Printer.Printf("error with shutdown: %v", err)
+		}
+	}()
+
+	if err := dependencies.Server.ListenAndServe(); err != http.ErrServerClosed {
+		// error with starting or closing listeners
+		dependencies.Printer.Printf("error with listening and serving: %v", err)
+		return false
+	}
+
+	waiter.Wait()
+	return ok
+}
---
+++ gateways/http/httputils/run_server_test.go
@@ -0,0 +1,151 @@
+package httputils
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"testing"
+	"testing/iotest"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRunServer(test *testing.T) {
+	type args struct {
+		shutdownCtx      context.Context
+		dependencies     RunServerDependencies
+		interruptSignals []os.Signal
+	}
+
+	for _, data := range []struct {
+		name   string
+		args   args
+		action func(test *testing.T)
+		wantOk assert.BoolAssertionFunc
+	}{
+		{
+			name: "success",
+			args: args{
+				shutdownCtx: context.Background(),
+				dependencies: RunServerDependencies{
+					Server: func() Server {
+						server := new(MockServer)
+						server.On("ListenAndServe").Return(http.ErrServerClosed)
+						server.
+							On(
+								"Shutdown",
+								mock.MatchedBy(func(context.Context) bool { return true }),
+							).
+							Return(nil)
+
+						return server
+					}(),
+					Printer: new(MockPrinter),
+				},
+				interruptSignals: []os.Signal{os.Interrupt},
+			},
+			action: func(test *testing.T) {
+				time.Sleep(time.Second)
+
+				currentProcess, err := os.FindProcess(os.Getpid())
+				require.NoError(test, err)
+
+				err = currentProcess.Signal(os.Interrupt)
+				require.NoError(test, err)
+			},
+			wantOk: assert.True,
+		},
+		{
+			name: "error on the ListenAndServe() call",
+			args: args{
+				shutdownCtx: context.Background(),
+				dependencies: RunServerDependencies{
+					Server: func() Server {
+						server := new(MockServer)
+						server.On("ListenAndServe").Return(iotest.ErrTimeout)
+
+						return server
+					}(),
+					Printer: func() Printer {
+						printer := new(MockPrinter)
+						printer.
+							On(
+								"Printf",
+								mock.MatchedBy(func(string) bool { return true }),
+								iotest.ErrTimeout,
+							).
+							Return()
+
+						return printer
+					}(),
+				},
+				interruptSignals: []os.Signal{os.Interrupt},
+			},
+			action: func(test *testing.T) {},
+			wantOk: assert.False,
+		},
+		{
+			name: "error on the Shutdown() call",
+			args: args{
+				shutdownCtx: context.Background(),
+				dependencies: RunServerDependencies{
+					Server: func() Server {
+						server := new(MockServer)
+						server.On("ListenAndServe").Return(http.ErrServerClosed)
+						server.
+							On(
+								"Shutdown",
+								mock.MatchedBy(func(context.Context) bool { return true }),
+							).
+							Return(iotest.ErrTimeout)
+
+						return server
+					}(),
+					Printer: func() Printer {
+						printer := new(MockPrinter)
+						printer.
+							On(
+								"Printf",
+								mock.MatchedBy(func(string) bool { return true }),
+								iotest.ErrTimeout,
+							).
+							Return()
+
+						return printer
+					}(),
+				},
+				interruptSignals: []os.Signal{os.Interrupt},
+			},
+			action: func(test *testing.T) {
+				time.Sleep(time.Second)
+
+				currentProcess, err := os.FindProcess(os.Getpid())
+				require.NoError(test, err)
+
+				err = currentProcess.Signal(os.Interrupt)
+				require.NoError(test, err)
+			},
+			wantOk: assert.False,
+		},
+	} {
+		test.Run(data.name, func(test *testing.T) {
+			go data.action(test)
+
+			gotOk := RunServer(
+				data.args.shutdownCtx,
+				data.args.dependencies,
+				data.args.interruptSignals...,
+			)
+
+			mock.AssertExpectationsForObjects(
+				test,
+				data.args.dependencies.Server,
+				data.args.dependencies.Printer,
+			)
+			data.wantOk(test, gotOk)
+		})
+	}
+}
