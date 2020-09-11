86e2ba08b0677fc1412916f93d3ccffcaa012d16
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-11 23:00:00 +0300

    Fix the storage.LinkSetter.SetLink() method

--- gateways/storage/link_setter.go
+++ gateways/storage/link_setter.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/thewizardplusplus/go-link-shortener-backend/entities"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // LinkSetter ...
@@ -16,10 +18,19 @@ type LinkSetter struct {
 
 // SetLink ...
 func (setter LinkSetter) SetLink(link entities.Link) error {
+	// by the time of setting the database may already have a link created
+	// in another thread; therefore, to avoid duplicates, we don't insert
+	// but update in the upsert mode; a link code is always unique, so we search
+	// by a link URL
 	_, err := setter.Client.innerClient.
 		Database(setter.Database).
 		Collection(setter.Collection).
-		InsertOne(context.Background(), link)
+		UpdateOne(
+			context.Background(),
+			bson.M{"url": link.URL},
+			bson.M{"$setOnInsert": bson.M{"code": link.Code}},
+			options.Update().SetUpsert(true),
+		)
 	if err != nil {
 		return errors.Wrap(err, "unable to set the link in MongoDB")
 	}
