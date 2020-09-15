a6b0d7ea707b7e1110349707a7f0c7f7e4a35df7
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-15 23:00:00 +0300

    Fix tests of the storage.NewClient() function

--- gateways/storage/client_test.go
+++ gateways/storage/client_test.go
@@ -1,42 +1,102 @@
-// +build ignore
+// +build integration
 
 package storage
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type Index struct {
+	Name      string `bson:"name"`
+	Namespace string `bson:"ns"`
+	Key       bson.M `bson:"key"`
+	Unique    bool   `bson:"unique"`
+}
 
 func TestNewClient(test *testing.T) {
 	type args struct {
-		uri string
+		uri        string
+		database   string
+		collection string
 	}
 
 	for _, data := range []struct {
-		name       string
-		args       args
-		wantClient assert.ValueAssertionFunc
-		wantErr    assert.ErrorAssertionFunc
+		name        string
+		args        args
+		wantClient  require.ValueAssertionFunc
+		wantIndexes []Index
+		wantErr     assert.ErrorAssertionFunc
 	}{
 		{
-			name:       "success",
-			args:       args{"mongodb://localhost:27017"},
-			wantClient: assert.NotNil,
-			wantErr:    assert.NoError,
+			name: "success",
+			args: args{
+				uri:        "mongodb://localhost:27017",
+				database:   "database",
+				collection: "collection",
+			},
+			wantClient: require.NotNil,
+			wantIndexes: []Index{
+				{
+					Name:      "_id_",
+					Namespace: "database.collection",
+					Key:       bson.M{"_id": int32(1)},
+					Unique:    false,
+				},
+				{
+					Name:      "code_1",
+					Namespace: "database.collection",
+					Key:       bson.M{"code": int32(1)},
+					Unique:    true,
+				},
+				{
+					Name:      "url_1",
+					Namespace: "database.collection",
+					Key:       bson.M{"url": int32(1)},
+					Unique:    true,
+				},
+			},
+			wantErr: assert.NoError,
 		},
 		{
-			name:       "error",
-			args:       args{":"},
-			wantClient: assert.Nil,
-			wantErr:    assert.Error,
+			name: "error",
+			args: args{
+				uri:        ":",
+				database:   "database",
+				collection: "collection",
+			},
+			wantClient:  require.Nil,
+			wantIndexes: nil,
+			wantErr:     assert.Error,
 		},
 	} {
 		test.Run(data.name, func(test *testing.T) {
-			gotClient, gotErr := NewClient(data.args.uri)
+			gotClient, gotErr :=
+				NewClient(data.args.uri, data.args.database, data.args.collection)
 
 			data.wantClient(test, gotClient.innerClient)
 			data.wantErr(test, gotErr)
+			if gotErr != nil {
+				return
+			}
+
+			cursor, err := gotClient.innerClient.
+				Database(data.args.database).
+				Collection(data.args.collection).
+				Indexes().
+				List(context.Background(), options.ListIndexes())
+			require.NoError(test, err)
+
+			var indexes []Index
+			err = cursor.All(context.Background(), &indexes)
+			require.NoError(test, err)
+
+			assert.ElementsMatch(test, data.wantIndexes, indexes)
 		})
 	}
 }
